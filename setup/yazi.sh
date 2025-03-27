677420e7d5212e99f91a044c836ef1e674b02385
mateuszrzeszutek <rzeszut@pm.me>
2025-03-27 19:44:15 +0100

    fix git config; add yazi git plugin

--- setup/yazi.sh
+++ setup/yazi.sh
@@ -9,22 +9,21 @@ install_yazi() {
   mkdir -p "$HOME/.config/yazi"
 }
 
-install_yazi_flavors() {
-  echo_yellow '>>> Configuring yazi ...'
-  mkdir -p "$HOME/.config/yazi/flavors"
-  if [[ ! -d "$HOME/.config/yazi/flavors/tokyo-night.yazi" ]]
-  then
-    git clone https://github.com/BennyOe/tokyo-night.yazi.git "$HOME/.config/yazi/flavors/tokyo-night.yazi"
-  fi
+install_yazi_plugins() {
+  echo_yellow '>>> Installing yazi addons ...'
+  ya pack -a BennyOe/tokyo-night
+  ya pack -a yazi-rs/plugins:git
 }
 
 configure_yazi() {
   echo_yellow '>>> Configuring yazi ...'
   mkdir -p "$HOME/.config/yazi"
+  ln -fs "$BASEDIR/config/yazi/init.lua" "$HOME/.config/yazi/init.lua"
+  ln -fs "$BASEDIR/config/yazi/yazi.toml" "$HOME/.config/yazi/yazi.toml"
   ln -fs "$BASEDIR/config/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
 }
 
 install_yazi
-install_yazi_flavors
+install_yazi_plugins
 configure_yazi
 
