4eea6b44bc4654d4816926712c1dea8992a640a8
mateuszrzeszutek <rzeszut@pm.me>
2025-04-01 19:23:32 +0200

    more yazi plugins

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,2 +1,3 @@
+require("full-border"):setup()
 require("git"):setup()
-
+require("smart-enter"):setup {}
--- setup/yazi.sh
+++ setup/yazi.sh
@@ -12,15 +12,18 @@ install_yazi() {
 install_yazi_plugins() {
   echo_yellow '>>> Installing yazi addons ...'
   ya pack -a BennyOe/tokyo-night
+  ya pack -a yazi-rs/plugins:full-border
   ya pack -a yazi-rs/plugins:git
+  ya pack -a yazi-rs/plugins:smart-enter
 }
 
 configure_yazi() {
   echo_yellow '>>> Configuring yazi ...'
   mkdir -p "$HOME/.config/yazi"
   ln -fs "$BASEDIR/config/yazi/init.lua" "$HOME/.config/yazi/init.lua"
-  ln -fs "$BASEDIR/config/yazi/yazi.toml" "$HOME/.config/yazi/yazi.toml"
+  ln -fs "$BASEDIR/config/yazi/keymap.toml" "$HOME/.config/yazi/keymap.toml"
   ln -fs "$BASEDIR/config/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
+  ln -fs "$BASEDIR/config/yazi/yazi.toml" "$HOME/.config/yazi/yazi.toml"
 }
 
 install_yazi
