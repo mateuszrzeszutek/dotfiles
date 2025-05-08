c37932b63b277826d7d0329e649e89022edbea0a
mateuszrzeszutek <rzeszut@pm.me>
2025-05-08 12:59:54 +0200

    minor nvim update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -206,14 +206,14 @@ require("neorg").setup({
 
 -- language servers
 require('mason').setup()
-require('mason-lspconfig').setup({
+require('mason-lspconfig').setup {
   ensure_installed = {
     'clangd',
     'gopls',
     'lua_ls',
     'rust_analyzer'
   },
-})
+}
 
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
