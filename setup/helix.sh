354c13c2ac7006d1f98bddbc37d04dc88d28c45c
mateuszrzeszutek <rzeszut@pm.me>
2026-01-09 17:39:06 +0100

    helix languages conf

--- setup/helix.sh
+++ setup/helix.sh
@@ -4,14 +4,17 @@ source "$BASEDIR/setup/_common.sh"
 
 install_helix() {
   echo_yellow ">>> Installing helix..."
-  is_executable brew && install__brew helix
-  is_executable dnf && install__dnf helix
+  is_executable brew &&\
+   install__brew helix bash-language-server shellcheck
+  is_executable dnf &&\
+   install__dnf helix nodejs-bash-language-server shellcheck
 }
 
 configure_helix() {
   echo_yellow ">>> Configuring helix ..."
   mkdir -p "$HOME/.config/helix"
   ln -fs "$BASEDIR/config/helix/config.toml" "$HOME/.config/helix/config.toml"
+  ln -fs "$BASEDIR/config/helix/languages.toml" "$HOME/.config/helix/languages.toml"
 }
 
 install_helix
